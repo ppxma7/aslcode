f4283f12470840accf227e3114d86e88ddb789ec
Michael Asghar <michael.asghar@nottingham.ac.uk>
2021-03-25 09:51:52 +0000

    update mni conversion script

--- ASL_diffav2mni.sh
+++ ASL_diffav2mni.sh
@@ -9,9 +9,9 @@ anatlist="m001_H03 m001_H07 m001_H08 m001_H09 m001_H11 m001_H13 m001_H14 m001_H1
     msub-003 msub-004 msub-005 msub-006 msub-008 msub-011 msub-012 msub-014 msub-020 msub-021 msub-022 msub-024 msub-025 msub-026\
     msub-027 msub-028 msub-031 msub-032 msub-033 msub-034 msub-038"  
 
-#anatlist="msub-038"
+#anatlist="m001_H08"
 
-# subjectlist="001_H07_V2 001_H08_V1 001_H08_V2 001_H09_V1 001_H09_V2 001_H11_V1 001_H11_V2 001_H13_V1 001_H13_V2\
+# subjectlist="001_H07_V1 001_H07_V2 001_H08_V1 001_H08_V2 001_H09_V1 001_H09_V2 001_H11_V1 001_H11_V2 001_H13_V1 001_H13_V2\
 #     001_H14_V1 001_H14_V2 001_H15_V1 001_H15_V2 001_H16_V1 001_H16_V2 001_H17_V1 001_H19_V1 001_H19_V2\
 #     001_H23_V1 001_H23_V2 001_H24_V1 001_H24_V2 001_H25_V1 001_H25_V2 001_H27_V1 001_H27_V2 001_H28_V1 001_H28_V2\
 #     001_H29_V1 001_H29_V2 001_H30_V1 001_H30_V2"
@@ -26,6 +26,10 @@ subjectlist="001_P01_V1 001_P01_V2 001_P02_V1 001_P02_V2 001_P03_V1 001_P03_V2 0
 # ISSUE with 001_P17_V2 PARRREC files are corrupted
 # H17 no data
 
+#
+#anatlist="m001_H07"
+#subjectlist="001_H08_V1"
+
 subjectlistJord="003 004 005 006 008 011 012 014 020 021 022 024 025 026 027 028 032 033 034 038"
 
 
@@ -37,8 +41,7 @@ MOUNTjord="/Volumes/ares/data/IBD/ASL/ASL_jord/"
 
 ANATMOUNT="/Volumes/ares/data/IBD/STRUCTURAL/bias_corrected/"
 
-
-# First, we should align all anatomicals to MNI SPACE
+#First, we should align all anatomicals to MNI SPACE
 # for anatsub in $anatlist
 # do
 # 	echo ${anatsub}.nii
@@ -57,13 +60,21 @@ ANATMOUNT="/Volumes/ares/data/IBD/STRUCTURAL/bias_corrected/"
 # 		-searchrz -90 90\
 # 		-dof 12\
 # 		-interp trilinear
+
+# 	echo "Running fnirt now..."
+# 	fnirt --ref=/usr/local/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz\
+# 		--in=${ANATMOUNT}/${anatsub}_brain_mni.nii.gz\
+# 		--iout=${ANATMOUNT}/${anatsub}_brain_mni_fnirt.nii.gz\
+# 		--cout=${ANATMOUNT}/${anatsub}_brain_mni_fnirt_transform\
+		
 # done
 
-# return
+
+##--aff=${ANATMOUNT}/${anatsub}_brain_mni.mat\
 
 cd ${MOUNT}
 
-# THEN, GO THROUGH ALL epi data and align these to their respective anatomicals
+#THEN, GO THROUGH ALL epi data and align these to their respective anatomicals
 
 for anatsub in $anatlist
 do
@@ -73,11 +84,11 @@ do
 	for subject in $subjectlist
 	do
 		SUBSTRING=${subject:3:4} #change this to 0:3 for the patients,
-		echo $SUBSTRING
+		#echo $SUBSTRING
 		if [[ "$anatsub" =~ "$SUBSTRING" ]]; then
 			echo "We gotta match" 
 			echo "${subject}"
-
+			echo "${anatsub}"
 			# In this case, actually better to use FLIRT
 
 			#echo "Align EPI to Anatomical with 12 DOF..."
@@ -112,41 +123,18 @@ do
 					-out $tmpPath2/diffav_calib_flirt_mni\
 					-applyxfm -init ${ANATMOUNT}/${anatsub}_brain_mni.mat
 			done
+
+			for file3 in $(find ${MOUNT}/${subject}/ -name 'diffav_calib_flirt_mni.nii.gz' );
+			do
+				tmpPath3=$(dirname $file3)
+				applywarp --ref=/usr/local/fsl/data/standard/MNI152_T1_2mm_brain\
+					--in=$file3\
+					--warp=${ANATMOUNT}/${anatsub}_brain_mni_fnirt_transform\
+					--out=$tmpPath3/diffav_calib_flirt_mni_fnirt
+
+			done
 			
 			cd ${MOUNT}
-
-			# 				echo $file
-			# 	flirt -in ${MOUNT}/${subject}/${subject}_WIPASL17a_aslpp/diffav_calib.nii.gz\
-			# 		-ref ${ANATMOUNT}/${anatsub}.nii\
-			# 		-out ${MOUNT}${subject}/${subject}_WIPASL17a_aslpp/diffav_calib_flirt\
-			# 		-omat ${MOUNT}${subject}/${subject}_WIPASL17a_aslpp/diffav_calib_flirt.mat\
-			# 		-bins 256\
-			# 		-cost corratio\
-			# 		-searchrx -90 90\
-			# 		-searchry -90 90\
-			# 		-searchrz -90 90\
-			# 		-dof 12\
-			# 		-interp trilinear
-
-
-			# # now apply transform to move EPI to MNI space
-			# 	echo "Align EPI to standard using Anatomical transformation..."
-			# 	flirt -in ${MOUNT}${subject}/${subject}_WIPASL17a_aslpp/diffav_calib_flirt\
-			# 		-ref /usr/local/fsl/data/standard/MNI152_T1_2mm_brain\
-			# 		-out ${MOUNT}${subject}/${subject}_WIPASL17a_aslpp/diffav_calib_flirt_mni\
-			# 		-applyxfm -init ${ANATMOUNT}/${anatsub}_brain_mni.mat
-
-
-			# ORIGINAL AFNI CODE
-			#echo "Aligning centers..."
-			#@align_centers -base ${ANATMOUNT}/${anatsub}.nii -dset ${MOUNT}/${subject}/${subject}_WIPASL17a_aslpp/diffav_calib.nii.gz
-
-			#echo "Running align_epi_anat script..."
-			#cd ${MOUNT}/${subject}/${subject}_WIPASL17a_aslpp/
-
-			#align_epi_anat.py -anat ${ANATMOUNT}/${anatsub}.nii -epi ${MOUNT}/${subject}/${subject}_WIPASL17a_aslpp/diffav_calib.nii.gz -epi_base 0 -epi2anat
-			#echo "Now converting to Nifti..."
-			#3dAFNItoNIFTI ${MOUNT}/${subject}/${subject}_WIPASL17a_aslpp/diffav_calib_al+orig
 		fi
 	done
 done
@@ -157,8 +145,3 @@ done
 
 
 
-# for subject in $subjectlist
-# do
-# 	fslorient -forceradiological HDREMODEL_${subject}_RCR_mc.nii
-# 	echo ${subject}
-# done
